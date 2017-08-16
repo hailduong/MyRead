c01baddd27c97cbf8d48732aee4da3624d6c09fd
Liano <hailyduong@gmail.com>
2017-08-17 00:42:36 +0700

    Update: Remove TODOs

--- src/App.js
+++ src/App.js
@@ -10,23 +10,11 @@ const $ = window.$;
 import HomePage from "./components/HomePage";
 import SearchPage from "./components/SearchPage";
 
-
-// TODO: Remove this; Exposing currently for testing on browser console.
-window.BooksAPI = BooksAPI;
-
-
 class BooksApp extends React.Component {
-
-	// TODO: Add loading state
+	
 	// TODO: Add "no book" state
 
 	state = {
-		/**
-		 * TODO: Instead of using this state variable to keep track of which page
-		 * we're on, use the URL in the browser's address bar. This will ensure that
-		 * users can use the browser's back and forward buttons to navigate between
-		 * pages, as well as provide a good URL they can bookmark and share.
-		 */
 		showSearchPage: false,
 		loading:true,
 		shelves: [
@@ -58,8 +46,6 @@ class BooksApp extends React.Component {
 
 	getAllBooks() {
 		let self = this;
-		// TODO: Remove this; Exposing currently for testing on browser console only.
-		window.bookApp = this;
 
 		BooksAPI.getAll().then(bookArray => {
 
